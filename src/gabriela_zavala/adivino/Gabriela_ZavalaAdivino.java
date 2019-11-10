52ab5e1bdfd89913eda24c56a359f394e2896a50
Propietario <Propietario@pc-1559579848>
2019-11-10 17:55:55 +0100

    límite 10 y 50

--- src/gabriela_zavala/adivino/Gabriela_ZavalaAdivino.java
+++ src/gabriela_zavala/adivino/Gabriela_ZavalaAdivino.java
@@ -11,8 +11,8 @@ import java.util.Scanner;
 public class Gabriela_ZavalaAdivino {
     
     public static void main(String[] args) {
-          int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
+          int n = 10,cont = 0;
+        int x = (int) (50 *Math.random());
         //System.out.print(x);
         Scanner sc = new Scanner(System.in);
         do {
