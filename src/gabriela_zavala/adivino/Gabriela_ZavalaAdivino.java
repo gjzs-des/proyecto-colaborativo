5ebdbb1522c5767354ffd4e874a5f01e0ee97f1f
Propietario <Propietario@pc-1559579848>
2019-11-10 14:16:23 +0100

    original

---
+++ src/gabriela_zavala/adivino/Gabriela_ZavalaAdivino.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gabriela_zavala.adivino;
+
+import java.util.Scanner;
+
+
+public class Gabriela_ZavalaAdivino {
+    
+    public static void main(String[] args) {
+          int n = 0,cont = 0;
+        int x = (int) (500 *Math.random());
+        //System.out.print(x);
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("\nIntroduce el valor de X: ");
+            n = sc.nextInt();
+            if (n > x) {
+                System.out.print("Te pasaste Amigo");
+            } else if (n < x){
+                System.out.print("Casi cerca ...");
+            }
+            cont++;
+        } while (n != x);
+        System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
+        sc.close();
+    }
+    
+}
